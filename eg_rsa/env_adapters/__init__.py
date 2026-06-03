93d844e3812bc44d2bd00a66c12068d773b905a9
Xiao Fu <128346161+Nicole-ying@users.noreply.github.com>
2026-06-03 21:10:23 +0800

    Fix environment adapter exports

--- eg_rsa/env_adapters/__init__.py
+++ eg_rsa/env_adapters/__init__.py
@@ -1,3 +1,3 @@
-from eg_rsa.env_adapters.lunar_lander_adapter import LunarLanderAdapter
+from eg_rsa.env_adapters.box_obs_adapter import BoxObsAdapter
 
-__all__ = ["LunarLanderAdapter"]
+__all__ = ["BoxObsAdapter"]
