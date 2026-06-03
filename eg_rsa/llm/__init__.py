25a6b2a064a282c00a55bbb4c3c60b444fe21de3
Xiao Fu <128346161+Nicole-ying@users.noreply.github.com>
2026-06-03 21:33:37 +0800

    Export EG-RSA LLM clients

--- eg_rsa/llm/__init__.py
+++ eg_rsa/llm/__init__.py
@@ -1,5 +1,13 @@
+from eg_rsa.llm.client_factory import build_llm_client
 from eg_rsa.llm.edit_agent import EditAgent
 from eg_rsa.llm.edit_prompt import build_edit_prompt
 from eg_rsa.llm.json_parser import extract_json_object
+from eg_rsa.llm.ollama_client import OllamaClient
 
-__all__ = ["EditAgent", "build_edit_prompt", "extract_json_object"]
+__all__ = [
+    "EditAgent",
+    "build_edit_prompt",
+    "extract_json_object",
+    "OllamaClient",
+    "build_llm_client",
+]
