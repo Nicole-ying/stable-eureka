257ddbceda956fa96bbec9b12b0518f099fead65
Xiao Fu <128346161+Nicole-ying@users.noreply.github.com>
2026-06-03 21:16:21 +0800

    Export EG-RSA diagnostics helpers

--- eg_rsa/diagnostics/__init__.py
+++ eg_rsa/diagnostics/__init__.py
@@ -1,4 +1,13 @@
 from eg_rsa.diagnostics.attribution import RewardAttributionAnalyzer
+from eg_rsa.diagnostics.event_evaluator import EventEvaluator
 from eg_rsa.diagnostics.hack_detectors import RewardHackDetector
+from eg_rsa.diagnostics.task_metrics import TaskMetricEvaluator
+from eg_rsa.diagnostics.trajectory_recorder import TrajectoryRecorder
 
-__all__ = ["RewardAttributionAnalyzer", "RewardHackDetector"]
+__all__ = [
+    "RewardAttributionAnalyzer",
+    "RewardHackDetector",
+    "TaskMetricEvaluator",
+    "EventEvaluator",
+    "TrajectoryRecorder",
+]
