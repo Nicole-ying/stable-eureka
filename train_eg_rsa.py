ac832520b89da790058fd030e6afbb62587b0fcd
Xiao Fu <128346161+Nicole-ying@users.noreply.github.com>
2026-06-04 21:46:06 +0800

    Add config argument to EG-RSA entrypoint

--- train_eg_rsa.py
+++ train_eg_rsa.py
@@ -1,6 +1,22 @@
+from __future__ import annotations
+
+import argparse
+
 from eg_rsa import EGRSARunner
 
 
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Run EG-RSA reward search experiments.")
+    parser.add_argument(
+        "--config",
+        type=str,
+        default="configs/eg_rsa_minimal.yml",
+        help="Path to an EG-RSA YAML config file.",
+    )
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    runner = EGRSARunner(config_path="./configs/eg_rsa_minimal.yml")
+    args = parse_args()
+    runner = EGRSARunner(config_path=args.config)
     runner.run()
