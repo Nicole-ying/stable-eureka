39bdbe8881334cf505460da330b495049d6d4226
Xiao Fu <128346161+Nicole-ying@users.noreply.github.com>
2026-06-04 08:55:44 +0800

    Export EG-RSA experiment summary

--- eg_rsa/evaluation/__init__.py
+++ eg_rsa/evaluation/__init__.py
@@ -1,3 +1,4 @@
+from eg_rsa.evaluation.experiment_summary import ExperimentSummary
 from eg_rsa.evaluation.posthoc_evaluator import PosthocEvaluator
 
-__all__ = ["PosthocEvaluator"]
+__all__ = ["PosthocEvaluator", "ExperimentSummary"]
