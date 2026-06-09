d54dbd99ee47c7251182c8bbe06e5fc2c0cbaee3
Xiao Fu <128346161+Nicole-ying@users.noreply.github.com>
2026-06-09 14:18:33 +0800

    Add v1 tool package

---
+++ eg_rsa/tools/__init__.py
@@ -0,0 +1,6 @@
+"""Tool-augmented semantic diagnostics for EG-RSA v1.
+
+These tools are intentionally read-only or proposal-auditing helpers. They do
+not directly mutate reward schemas or training state. Runner/AgentActionController
+will decide when to call them.
+"""
